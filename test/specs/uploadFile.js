bd7626a20dfdf618fa09d06bd8cb433a7310e5f9
automationbro <automationbro@gmail.com>
2020-07-12 15:31:57 -0400

    Add comments in the uploadFile example

--- test/specs/uploadFile.js
+++ test/specs/uploadFile.js
@@ -2,14 +2,25 @@ const path = require('path');
 
 describe('Upload File', () => {
   it('should be able to upload a file', () => {
+    // find selectors
     const input = $('#file-upload');
     const submitBtn = $('#file-submit');
+
+    // store test file path
     const filePath = path.join(__dirname, '../data/chrome.png');
+
+    // use browser.uploadFile to upload the test file
     const remoteFilePath = browser.uploadFile(filePath);
 
+    // access the test page
     browser.url('/upload');
+
+    // set file path value in the input field
     input.setValue(remoteFilePath);
-    submitBtn.click();
-    browser.pause(2000); // chai assertion
+    submitBtn.click(); // click the submit button
+
+    browser.pause(2000); // temporary pause to see if the file got upload successfully
+
+    // Add your assertion here
   });
 });
